d992dc2b43b0172d9b5b9b9bdc45db82b9184308
Ajit <avadukoot@seic.com>
2019-01-15 14:17:13 +0530

    my app 1 push

---
+++ my-app1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { LoginModule } from './login/login.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    SearchBoxDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
